b9150b0b06effbf1ad2eaa0c60126a8ce91b7b2b
wcarbajal <wcarbajal@outlook.com>
2025-07-21 17:55:20 -0500

    21

--- src/components/procesos/ListaProcesos.jsx
+++ src/components/procesos/ListaProcesos.jsx
@@ -16,6 +16,15 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import {
   Dialog,
@@ -32,6 +41,7 @@ import { Input } from '../ui/input';
 import { nuevoProcesoSchema } from './schema/SchemaProcesos';
 
 
+
 export const ListaProcesos = () => {
 
   const form = useForm( {
@@ -40,38 +50,37 @@ export const ListaProcesos = () => {
       codigo: "",
       nombre: "",
       descripcion: "",
+      nivel: "",
+      tipo: "",
       parentId: "", // Usa undefined para campos opcionales numéricos
     },
   } );
 
   const [ procesos, setProcesos ] = useState( [] );
-  /* const [ nuevoProceso, setNuevoProceso ] = useState( {
-    
-    codigo: '',
-    nombre: '',
-    descripcion: '',
-    parentId: ''
-  } ); */
 
-  const cargarProcesos = useCallback(async () => {
+
+  const cargarProcesos = useCallback( async () => {
     try {
-      const data = await fetchConToken('procesos');
-      console.log(data)
-      setProcesos(data.procesos);
-    } catch (error) {
-      console.error('Error fetching procesos:', error);
+      const data = await fetchConToken( 'procesos' );
+      console.log( data );
+      setProcesos( data.procesos );
+    } catch ( error ) {
+      console.error( 'Error fetching procesos:', error );
     }
-  }, []);
+  }, [] );
 
-  useEffect(() => {
+  useEffect( () => {
     cargarProcesos();
-  }, [cargarProcesos]);
+  }, [ cargarProcesos ] );
 
-  const onSubmit = ( values ) => {
+  const onSubmit = async ( values ) => {
 
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log( values );
+    // Enviar los datos al servidor
+    const respuesta = await fetchConToken( 'procesos/registrar', values, 'POST' );
+    console.log( respuesta );
   };
 
   return (
@@ -86,13 +95,40 @@ export const ListaProcesos = () => {
               <IconPlus className="" />
             </Button>
           </DialogTrigger>
-         
-          <DialogContent className="sm:max-w-[425px]">
-             <DialogHeader>
-            <DialogTitle className="text-center">Nuevo Proceso</DialogTitle>
-          </DialogHeader>
+
+          <DialogContent className="sm:max-w-[425px] " aria-describedby="dialog-description" >
+            <DialogHeader>
+              <DialogTitle className="text-center">Nuevo Proceso</DialogTitle>
+              <DialogDescription id="dialog-description">
+                Complete los datos para crear un nuevo proceso.
+              </DialogDescription>
+            </DialogHeader>
             <Form { ...form }>
               <form onSubmit={ form.handleSubmit( onSubmit ) } className="space-y-8">
+
+                <FormField
+                  control={ form.control }
+                  name="tipo"
+                  render={ ( { field } ) => (
+                    <FormItem >
+                      <FormLabel >Tipo de proceso</FormLabel>
+                      <Select onValueChange={ field.onChange } defaultValue={ field.value }>
+                        <FormControl className="w-full">
+                          <SelectTrigger>
+                            <SelectValue placeholder="Seleccione un tipo" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="Estratégico">Estratégico</SelectItem>
+                          <SelectItem value="Misional">Misional</SelectItem>
+                          <SelectItem value="Soporte">Soporte</SelectItem>
+                        </SelectContent>
+                      </Select>
+
+                      <FormMessage />
+                    </FormItem>
+                  ) }
+                />
                 <FormField
                   control={ form.control }
                   name="codigo"
@@ -106,6 +142,7 @@ export const ListaProcesos = () => {
                     </FormItem>
                   ) }
                 />
+
                 <FormField
                   control={ form.control }
                   name="nombre"
@@ -115,6 +152,37 @@ export const ListaProcesos = () => {
                       <FormControl>
                         <Input placeholder="Nombre del proceso" { ...field } />
                       </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  ) }
+                />
+
+                <FormField
+                  control={ form.control }
+                  name="nivel"
+                  render={ ( { field } ) => (
+                    <FormItem >
+                      <FormLabel >Nivel de proceso</FormLabel>
+                      <Select onValueChange={ field.onChange } defaultValue={ field.value }>
+                        <FormControl className="w-full">
+                          <SelectTrigger>
+                            <SelectValue placeholder="Seleccione un nivel" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="0">0</SelectItem>
+                          <SelectItem value="1">1</SelectItem>
+                          <SelectItem value="2">2</SelectItem>
+                          <SelectItem value="3">3</SelectItem>
+                          <SelectItem value="4">4</SelectItem>
+                          <SelectItem value="5">5</SelectItem>
+                          <SelectItem value="6">6</SelectItem>
+                          <SelectItem value="7">7</SelectItem>
+                          <SelectItem value="8">8</SelectItem>
+                          <SelectItem value="9">9</SelectItem>
+                        </SelectContent>
+                      </Select>
+
                       <FormMessage />
                     </FormItem>
                   ) }
@@ -132,15 +200,32 @@ export const ListaProcesos = () => {
                     </FormItem>
                   ) }
                 />
+
                 <FormField
                   control={ form.control }
                   name="parentId"
                   render={ ( { field } ) => (
-                    <FormItem>
-                      <FormLabel>ID Padre (opcional)</FormLabel>
-                      <FormControl>
-                        <Input type="number" placeholder="ID del proceso padre" { ...field } />
-                      </FormControl>
+                    <FormItem >
+                      <FormLabel >Proceso padre (opcional)</FormLabel>
+                      <Select onValueChange={ field.onChange } defaultValue={ field.value }>
+                        <FormControl className="w-full">
+                          <SelectTrigger>
+                            <SelectValue placeholder="Seleccione un tipo" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {
+                            procesos && procesos.length > 0 &&
+                            procesos.map( proceso => (
+                              <SelectItem key={ proceso.id } value={ proceso.id.toString() }>
+                                { proceso.codigo } - { proceso.nombre }
+                                <Button>delete</Button>
+                              </SelectItem>
+                            ) )
+                          }
+                        </SelectContent>
+                      </Select>
+
                       <FormMessage />
                     </FormItem>
                   ) }
@@ -161,12 +246,12 @@ export const ListaProcesos = () => {
           procesos.sort( ( a, b ) => a.codigo.localeCompare( b.codigo ) ).map( proceso => (
 
             <div key={ proceso.id } className="">
-              <span className="text-xs">{ proceso.codigo } - { proceso.nombre }</span>
+              <span className="text-xs">  { proceso.codigo } - { proceso.nombre }</span>
 
             </div>
           ) )
         }
-      </div>;
+      </div>
 
 
       {/*  <div className="p-2">
--- src/components/procesos/schema/SchemaProcesos.jsx
+++ src/components/procesos/schema/SchemaProcesos.jsx
@@ -5,6 +5,12 @@ export const nuevoProcesoSchema = z.object({
   codigo: z.string().min(2, "El código es obligatorio"),
   nombre: z.string().min(5, "El nombre es obligatorio"),
   descripcion: z.string().min(5, "La descripción es obligatoria"),
+  nivel: z.enum(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], {
+    errorMap: () => ({ message: "Nivel no válido" }),
+  }),
+  tipo: z.enum(["Misional", "Soporte", "Estratégico"], {
+    errorMap: () => ({ message: "Tipo de proceso no válido" }),
+  }),
   parentId: z.string()
   .regex(/^\d+$/, "El ID del padre debe ser un número entero positivo")
   .optional()
