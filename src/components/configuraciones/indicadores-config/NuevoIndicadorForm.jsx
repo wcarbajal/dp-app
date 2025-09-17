3486a2a38ee9964ee2235567531efb95df571643
wcarbajal <wcarbajal@outlook.com>
2025-09-17 17:57:13 -0500

    modificar indicador

--- src/components/configuraciones/indicadores-config/NuevoIndicadorForm.jsx
+++ src/components/configuraciones/indicadores-config/NuevoIndicadorForm.jsx
@@ -5,15 +5,19 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import { nuevoIndicadorSchema } from '@/schema/InidicadorSchema';
+import { useEffect } from 'react';
+import { fetchConToken } from '@/helpers/fetch';
 
 
-export const NuevoIndicadorForm = ({mapaId, indicadoresList}) => {
+export const NuevoIndicadorForm = ( { mapaId, indicadoresList } ) => {
 
-  console.log({indicadoresList});
-  
+  console.log( { indicadoresList } );
 
-  const form = useForm({
-    resolver: zodResolver(nuevoIndicadorSchema),
+
+
+
+  const form = useForm( {
+    resolver: zodResolver( nuevoIndicadorSchema ),
     defaultValues: {
       codigo: "",
       nombre: "",
@@ -21,11 +25,34 @@ export const NuevoIndicadorForm = ({mapaId, indicadoresList}) => {
       parentId: null,
       mapaId: mapaId || null,
     },
-  });
+  } );
 
-  const onSubmit = (data) => {
-    console.log(data);
-  }
+  // Escucha cambios en tipoNivel y actualiza el código
+  const tipoNivel = form.watch( "tipoNivel" );
+  // Nuevo campo para la parte editable del código
+  const codigoSufijo = form.watch( "codigoSufijo" ) || "";
+  console.log( { tipoNivel } );
+
+  // Cuando cambia tipoNivel, actualiza el código automáticamente
+  // Puedes personalizar la lógica para construir el código aquí
+  // Actualiza el valor de "codigo" cada vez que cambia tipoNivel o codigoSufijo
+  useEffect( () => {
+    if ( tipoNivel ) {
+      form.setValue( "codigo", `${ tipoNivel }.${ codigoSufijo }` );
+    } else {
+      form.setValue( "codigo", "" );
+    }
+  }, [ tipoNivel, codigoSufijo, form ] );
+
+
+  const onSubmit = async ( data ) => {
+    
+    console.log( data );
+
+    const respuesta = await fetchConToken( `indicador/${mapaId}`, data, 'POST' );
+
+    console.log( { respuesta } );
+  };
 
   return (
     <DialogHeader>
@@ -33,66 +60,84 @@ export const NuevoIndicadorForm = ({mapaId, indicadoresList}) => {
       <DialogDescription>
         Completa los datos para crear un nuevo indicador.
       </DialogDescription>
-      <Form {...form}>
+      <Form { ...form }>
         <form
-          onSubmit={form.handleSubmit(onSubmit)}
+          onSubmit={ form.handleSubmit( onSubmit ) }
           className="space-y-3 mt-4"
         >
+          <div className="grid grid-cols-2 gap-4">
+            <FormField
+              control={ form.control }
+              name="tipoNivel"
+              render={ ( { field } ) => (
+                <FormItem>
+                  <FormLabel>Tipo de Nivel</FormLabel>
+                  <FormControl>
+                    <select { ...field } className="w-full border rounded p-2">
+                      <option value="">...</option>
+                      <option value="OEI">OEI</option>
+                      <option value="AEI">AEI</option>
+                      <option value="IO">IO</option>
+                    </select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              ) }
+            />
+
+            <FormField
+              control={ form.control }
+              name="codigoSufijo"
+              render={ ( { field } ) => (
+                <FormItem>
+                  <FormLabel>Correlativo de indicador</FormLabel>
+                  <FormControl>
+                    <Input { ...field } autoFocus placeholder="Ingrese el sufijo del código" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              ) }
+            />
+          </div>
           <FormField
-            control={form.control}
+            control={ form.control }
             name="codigo"
-            render={({ field }) => (
+            render={ ( { field } ) => (
               <FormItem>
                 <FormLabel>Código</FormLabel>
                 <FormControl>
-                  <Input {...field} />
+                  <Input { ...field } readOnly />
                 </FormControl>
                 <FormMessage />
               </FormItem>
-            )}
+            ) }
           />
+
           <FormField
-            control={form.control}
+            control={ form.control }
             name="nombre"
-            render={({ field }) => (
+            render={ ( { field } ) => (
               <FormItem>
                 <FormLabel>Nombre</FormLabel>
                 <FormControl>
-                  <Input {...field} />
+                  <Input { ...field } />
                 </FormControl>
                 <FormMessage />
               </FormItem>
-            )}
+            ) }
           />
+
           <FormField
-            control={form.control}
-            name="tipoNivel"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tipo de Nivel</FormLabel>
-                <FormControl>
-                  <select {...field} className="w-full border rounded p-2">
-                    <option value="">Selecciona...</option>
-                    <option value="OEI">OEI</option>
-                    <option value="AEI">AEI</option>
-                    <option value="IO">IO</option>
-                  </select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-           <FormField
-            control={form.control}
+            control={ form.control }
             name="parentId"
-            render={({ field }) => (
+            render={ ( { field } ) => (
               <FormItem>
                 <FormLabel>Tipo de Nivel</FormLabel>
                 <FormControl>
-                  <select {...field} className="w-full border rounded p-2">
-                    <option value="">Selecciona...</option>
+                  <select { ...field } className="w-full border rounded p-2">
+                    <option value="">Sin padre</option>
                     { indicadoresList && indicadoresList.map( indicador => (
-                      <option key={ indicador.id } value={ indicador.id }>{ indicador.nombre }</option>
+                      <option key={ indicador.id } value={ indicador.id }>{ indicador.codigo } - { indicador.nombre }</option>
                     ) )
 
                     }
@@ -100,9 +145,9 @@ export const NuevoIndicadorForm = ({mapaId, indicadoresList}) => {
                 </FormControl>
                 <FormMessage />
               </FormItem>
-            )}
+            ) }
           />
-          {/* Agrega aquí más campos según tu schema */}
+          {/* Agrega aquí más campos según tu schema */ }
           <Button type="submit" className="w-full mt-2">
             Registrar
           </Button>
--- src/schema/InidicadorSchema.jsx
+++ src/schema/InidicadorSchema.jsx
@@ -4,7 +4,7 @@ import z from 'zod';
 export const nuevoIndicadorSchema = z.object( {
   codigo: z.string().min( 2, "Debe tener al menos 2 caracteres" ).max( 20, "Debe tener como máximo 20 caracteres" ),
   nombre: z.string().min( 2, "Campo requerido" ).max( 100, "Debe tener como máximo 100 caracteres" ),
-  tipoNivel: z.enum( [ "OEI", "AEI", "IP" ], "Selecciona una opción" ),
+  tipoNivel: z.enum( [ "OEI", "AEI", "IO" ], "Selecciona una opción" ),
   parentId: z.string().optional().nullable(),
   mapaId: z.number().optional().nullable(),
 } );
